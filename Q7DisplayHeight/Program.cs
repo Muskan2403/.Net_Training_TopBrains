dbc414f06c15d46eb72247a20f5f9f8713469705
Muskan2403 <muskandarji24@gmail.com>
2026-01-19 15:01:53 +0530

    Practice Question

---
+++ Q7DisplayHeight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Q7DisplayHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            if(height <= 0 || height > 300)
+            {
+                Console.WriteLine("Invalid height");
+                return;
+            }
+            else{
+                if(height  < 150)
+                {
+                    Console.WriteLine("Short");
+                }
+                else if(height >= 150 && height < 180)
+                {
+                    Console.WriteLine("Average");
+                }
+                else if(height >= 180)
+                {
+                    Console.WriteLine("Tall");
+                }
+            }
+            
+        }
+    }
+}
