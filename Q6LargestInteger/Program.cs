693ca17d5c0197be11c5360134fb20096d0a3c8d
Muskan2403 <muskandarji24@gmail.com>
2026-01-19 14:45:55 +0530

    Initial Commit

---
+++ Q6LargestInteger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Q6LargestInteger
+{
+    class Program{
+        public static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 > num2)
+            {
+                if(num1 > num3)
+                {
+                    Console.WriteLine($"{num1} is the Largest Integer");
+                }
+                else
+                {
+                    Console.WriteLine($"{num3} is the Largest Integer");
+
+                }
+            }
+            else
+            {
+                if(num2 > num3)
+                {
+                    Console.WriteLine($"{num2} is the Largest Integer");
+                }
+                else
+                {
+                    Console.WriteLine($"{num3} is the Largest Integer");
+                }
+            }
+
+
+        }
+    }
+}
