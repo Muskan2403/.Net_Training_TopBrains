5ae4f68d8986c01401a3985bd3b2ff8c3bf6c093
Muskan2403 <muskandarji24@gmail.com>
2026-01-20 15:23:32 +0530

    Practice Question

---
+++ Q25MidpointRounding/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Q25MidpointRounding
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double areaOfCircle = Area(radius);
+
+            Console.WriteLine($"Area is {areaOfCircle} cm²");
+        }
+
+        public static double Area(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            double result = Math.Round(area, 2, MidpointRounding.AwayFromZero);
+
+            return result;
+        }
+    }
+}
