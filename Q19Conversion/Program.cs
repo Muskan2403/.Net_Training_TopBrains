bf4f91ebc6117d96605c4f4a5ae456b87c9fd5f2
Muskan2403 <muskandarji24@gmail.com>
2026-01-20 14:59:11 +0530

    Practice Question

---
+++ Q19Conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Q19Conversion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Feet: ");
+            int feet = Convert.ToInt32(Console.ReadLine());
+
+            double centimeters = 30.48 * feet;
+            double result = Math.Round(centimeters,2,MidpointRounding.AwayFromZero);
+
+            Console.WriteLine($"{feet} Feet : {result}cm");
+        }
+    }
+}
