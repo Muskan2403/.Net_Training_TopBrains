826f4e63e756fd99b3ca2035de9aa0ba5b5e80e3
Muskan2403 <muskandarji24@gmail.com>
2026-02-03 15:46:17 +0530

    NUnit_Assessment

---
+++ NUnit_Assessment/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnit_Assessment
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void Test_Deposit_ValidAmount()
+        {
+            Program account = new Program(1000m);
+
+            account.Deposit(500m);
+
+            Assert.That(account.Balance, Is.EqualTo(1500m));
+        }
+
+        [Test]
+        public void Test_Deposit_NegativeAmount()
+        {
+            Program account = new Program(1000m);
+
+            var ex = Assert.Throws<Exception>(() => account.Deposit(-200m));
+
+            Assert.That(ex.Message, Is.EqualTo("Deposit amount cannot be negative"));
+        }
+
+        [Test]
+        public void Test_Withdraw_ValidAmount()
+        {
+            Program account = new Program(1000m);
+
+            account.Withdraw(300m);
+
+            Assert.That(account.Balance, Is.EqualTo(700m));
+        }
+
+        [Test]
+        public void Test_Withdraw_InsufficientFunds()
+        {
+            Program account = new Program(500m);
+
+            var ex = Assert.Throws<Exception>(() => account.Withdraw(800m));
+
+            Assert.That(ex.Message, Is.EqualTo("Insufficient funds."));
+        }
+    }
+}
