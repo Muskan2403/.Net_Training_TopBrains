a03b679eff483ea744b54d58a633ab402dfbb2cf
Muskan2403 <muskandarji24@gmail.com>
2026-02-03 15:10:08 +0530

    PracticeQuestionFactoryRobot

---
+++ FactoryRobotHazardAnalyzer/RobotSafetyException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FactoryRobotHazardAnalyzer{
+
+    public class RobotSafetyException : Exception
+    {
+        public RobotSafetyException(string message) : base(message)
+        {
+        }
+    }
+}
