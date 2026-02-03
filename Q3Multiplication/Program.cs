4f69cd0ac1b0c7d0ec99eea9c1f21004ef6d4333
Muskan2403 <muskandarji24@gmail.com>
2026-02-03 14:48:18 +0530

    PracticeQuestion3

---
+++ Q3Multiplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Q3Multiplication
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int upto = Convert.ToInt32(Console.ReadLine());
+
+            int[] row = new int[upto];
+
+            for (int i = 0; i < upto; i++)
+            {
+                row[i] = n * (i + 1);
+
+            }
+            Console.Write("[");
+
+            for(int i = 0;i< upto; i++)
+            {
+                Console.Write(row[i]);
+                if (i < upto - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
